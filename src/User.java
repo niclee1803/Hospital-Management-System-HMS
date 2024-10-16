8017758b9513d4798ffb027ee1b7d906848f4b68
niclee1803 <niclee1803@outlook.com>
2024-10-16 14:12:46 +0800

    Added data files

---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String userID;
+    private String password;
+
+    public User(String userID, String password) {
+        this.userID = userID;
+        this.password = password;
+    }
+
+
+}
