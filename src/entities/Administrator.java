53ad2266cb75bbe9f03acf83ea677a010c00aa91
leanneyeong <leannneyeong@gmail.com>
2024-11-19 18:01:28 +0800

    Modifidied java docs for AppointmentManager.java

--- src/entities/Administrator.java
+++ src/entities/Administrator.java
@@ -1,16 +1,34 @@
 package entities;
 
+/**
+ * Represents an Administrator, which is a subclass of User.
+ * An Administrator has additional attributes such as gender and age.
+ */
 public class Administrator extends User {
     private Gender gender;
     private int age;
 
+    /**
+     * Constructs a new Administrator with the specified ID, name, gender, and age.
+     *
+     * @param id     the unique identifier for the Administrator.
+     * @param name   the name of the Administrator.
+     * @param gender the gender of the Administrator.
+     * @param age    the age of the Administrator.
+     */
     public Administrator(String id, String name, Gender gender, int age) {
         super(id, name);
         this.gender = gender;
         this.age = age;
     }
 
-
+    /**
+     * Sets the name of the Administrator.
+     * Overrides the setName method from the User class.
+     *
+     * @param name the new name for the Administrator.
+     */
+    @Override
     public void setName(String name) {
         super.setName(name); // Use the User's setName method
     }
